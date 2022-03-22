37332e4f2899eff4f37e8c9d44578afeb122979d
drew99kt <drew99kt@gmail.com>
2022-03-22 00:23:49 -0400

    adding js stuff

---
+++ js/jsPractice.js
@@ -0,0 +1,18 @@
+let message;
+
+
+let name2= 'nat', 
+age=45,
+message2='gimmie all your money'; 
+
+alert(name2)
+
+let $=1;
+let _=2;
+alert($+_)
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+//https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/fundamentals-part-1#numbers
