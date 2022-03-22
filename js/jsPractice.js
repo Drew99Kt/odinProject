0746892c6fc8a3b04d73f7f1274828ffe7df5b31
drew99kt <drew99kt@gmail.com>
2022-03-22 02:02:11 -0400

    more js stuff

--- js/jsPractice.js
+++ js/jsPractice.js
@@ -1,18 +1,54 @@
-let message;
+// let message;
 
 
-let name2= 'nat', 
-age=45,
-message2='gimmie all your money'; 
+// let name2= 'nat', 
+// age=45,
+// message2='gimmie all your money'; 
 
-alert(name2)
+// // alert(name2)
 
-let $=1;
-let _=2;
-alert($+_)
+// // let $=1;
+// // let _=2;
+// // alert($+_)
 
-const COLOR_RED = "#F00";
-const COLOR_GREEN = "#0F0";
-const COLOR_BLUE = "#00F";
-const COLOR_ORANGE = "#FF7F00";
-//https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/fundamentals-part-1#numbers
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+
+
+// //left off here
+
+// //https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/fundamentals-part-1#numbers
+
+
+
+// const lotsOfDecimal = 1.766584958675746364;
+// lotsOfDecimal;
+// const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+// twoDecimalPlaces;
+
+// console.log(twoDecimalPlaces)
+
+
+// if (1===2) {
+//     console.log("its true");
+// } else {
+//     console.log("its false");
+// }
+
+const btn= document.querySelector("#btnClass");
+console.log(btn)
+btn.addEventListener("click", updateBtn);
+
+
+
+function updateBtn() {
+    if (btn.textContent==='click me') {
+        btn.textContent= 'dont touch me'
+    } else if (btn.textContent==='dont touch me') {
+        btn.textContent= 'click me'
+    }
+    console.log("i was callled")
+}
+
