551821c2146fb281b6c269cab163e7c66879b32c
drew99kt <drew99kt@gmail.com>
2022-04-03 22:58:03 -0400

    addding drawingp

---
+++ js/etchJS.js
@@ -0,0 +1,69 @@
+
+
+
+
+ 
+
+
+//    const container2 = document.querySelector('.body');
+//    const content2 = document.createElement('button');
+//    content2.classList.add('box');
+//    content2.textContent = 'box';
+//    container2.appendChild(content2);
+
+
+for (let i1 = 0; i1 < 32; i1++) {
+    for (let i = 0; i <60 ; i++) {
+        const container = document.querySelector('.body');
+        const content = document.createElement('button');
+        content.classList.add('box');
+        content.textContent = "";
+        container.appendChild(content);
+    
+    
+        // const container = document.querySelector('.container');
+        // const content = document.createElement('button');
+        // content.classList.add('box');
+        // content.textContent = 'box'+(i+1);
+        // container.appendChild(content);
+    }
+    const container = document.querySelector('.body');
+    const content = document.createElement('br');
+    content.classList.add('break');
+    content.textContent = "";
+    container.appendChild(content);
+}
+
+
+
+  let mouseDown = 0;
+  document.body.onmousedown = function() { 
+    ++mouseDown;
+    console.log("mounse down")
+    
+  }
+  document.body.onmouseup = function() {
+    --mouseDown;
+  }
+
+let x2= function(){
+    while (mouseDown==1){
+    console.log("holding down")
+    x;
+    }
+}
+
+    let boxs= document.getElementsByClassName('box');
+    console.log(boxs)
+ 
+
+
+ for (let i = 0; i < boxs.length; i++) {
+    boxs[i].addEventListener('mouseover', function(e){
+        console.log(e.target);    
+        if (e.target.classList=='box' && mouseDown===1) {
+                e.target.style.backgroundColor='black';
+                console.log(e.target)
+            }
+    })
+ }
--- js/jsPractice.js
+++ js/jsPractice.js
@@ -1,86 +1,57 @@
-// let message;
 
-
-// let name2= 'nat', 
-// age=45,
-// message2='gimmie all your money'; 
-
-// // alert(name2)
-
-// // let $=1;
-// // let _=2;
-// // alert($+_)
-
-// const COLOR_RED = "#F00";
-// const COLOR_GREEN = "#0F0";
-// const COLOR_BLUE = "#00F";
-// const COLOR_ORANGE = "#FF7F00";
-
-
-// //left off here
-
-// //https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/fundamentals-part-1#numbers
+// const btn= document.querySelector("#btnClass");
+// console.log(btn)
+// btn.addEventListener("click", updateBtn);
 
 
 
-// const lotsOfDecimal = 1.766584958675746364;
-// lotsOfDecimal;
-// const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
-// twoDecimalPlaces;
-
-// console.log(twoDecimalPlaces)
-
-
-// if (1===2) {
-//     console.log("its true");
-// } else {
-//     console.log("its false");
+// function updateBtn() {
+//     if (btn.textContent==='click me') {
+//         btn.textContent= 'dont touch me'
+//     } else if (btn.textContent==='dont touch me') {
+//         btn.textContent= 'click me'
+//     }
+//     console.log("i was callled");
 // }
 
-const btn= document.querySelector("#btnClass");
-console.log(btn)
-btn.addEventListener("click", updateBtn);
 
+// function add7(num){
+//     return num+7
+// }
 
+// console.log(add7(1));
 
-function updateBtn() {
-    if (btn.textContent==='click me') {
-        btn.textContent= 'dont touch me'
-    } else if (btn.textContent==='dont touch me') {
-        btn.textContent= 'click me'
-    }
-    console.log("i was callled");
-}
+// function times(num) {
+//     return num*2;
+// }
+// console.log(times(1));
 
-
-function add7(num){
-    return num+7
-}
-
-console.log(add7(1));
-
-function times(num) {
-    return num*2;
-}
-console.log(times(1));
-
-function firstCap(string1) {
+// function firstCap(string1) {
   
-    return string1[0].toUpperCase()+string1.slice(1);
-}
+//     return string1[0].toUpperCase()+string1.slice(1);
+// }
 
-console.log(firstCap("helloworld"));
+// console.log(firstCap("helloworld"));
 
 
 
-function lastCap(input) {
+// function lastCap(input) {
 
-    let lastNum= input.length;
+//     let lastNum= input.length;
   
-    let lastIndex=input[lastNum-1].toUpperCase();
+//     let lastIndex=input[lastNum-1].toUpperCase();
   
-    return input.substring(0,lastNum-1)+lastIndex;
-}
-console.log(lastCap("marry"));
+//     return input.substring(0,lastNum-1)+lastIndex;
+// }
+// console.log(lastCap("marry"));
 
-let answer = parseInt(prompt("To access this site you need to enter password"));
+// let answer = parseInt(prompt("To access this site you need to enter password"));
+
+
+const h12= document.querySelector('#h1');
+const div2= document.createElement('div');
+div2.classList.add('tacos');
+div2.textContent='Hey im red';
+
+h12.appendChild(div2);
+
