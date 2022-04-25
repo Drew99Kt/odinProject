392c4a9d4614352bc08df8bfebbe1bc73a52d4b9
drew99kt <drew99kt@gmail.com>
2022-04-24 20:58:40 -0400

    basic calculator

--- js/calculatorJS.js
+++ js/calculatorJS.js
@@ -6,37 +6,79 @@ let disply2= document.getElementById("disply");
 let displyInput1="";
 let displyInput2="";
 let displyInput3="";
-
+let operation="";
+let opIndex=0;
 for (let i = 0; i < boxs.length; i++) {
     boxs[i].addEventListener('click', function(e){
-        console.log(e.target.textContent);
-    
+
+        // if(displyInput2!=null){
+        //     opIndex=2;
+      if (e.target.textContent==="") {
+          return;
+      }
+      
+         switch (e.target.textContent) {
+        case "+":
+            operation="+";
+            opIndex=1;
+            break;
+        case "-":
+            operation="-";
+            opIndex=1;
+            break;
+        case "/":
+            operation="/";
+            opIndex=1;
+            break;
+        case "x":
+            operation="x";
+            opIndex=1;
+            break;
+       
+      }
 
 
 
-
-
-        if (e.target.textContent==='CE') {
-            clearScreen();
-        
+          
+        if (e.target.textContent==="="){
+            //displyInput1 operation on displyInput2
+            output();
+            return;
         }
-        if (e.target.textContent!='+' && e.target.textContent!= 'CE') {
+        console.log(opIndex)
+        if (opIndex===0) {
             displyInput1+=e.target.textContent;
             disply2.placeholder=displyInput1;
-        }else if (e.target.textContent!= 'CE'){
-            displyInput2+=e.target.textContent;
+        }else if(opIndex===1){
+            displyInput2=e.target.textContent;
             disply2.placeholder=displyInput2;
+            opIndex=2;
+            return;
+        }else if(opIndex===2){
+            displyInput3+=e.target.textContent;
+            disply2.placeholder=displyInput3;
         }
-              
+
+
+        if (e.target.textContent==='CE')clearScreen();
+    
+        
+      
+     
        
 
-      
+        // if (e.target.textContent!='+' && e.target.textContent!= 'CE') {
+        //     displyInput1+=e.target.textContent;
+        //     disply2.placeholder=displyInput1;
+        // }else if (e.target.textContent!= 'CE'){
+        //     displyInput2+=e.target.textContent;
+        //     disply2.placeholder=displyInput2;
+        // }
 
- 
-        if (e.target.classList=='box' && mouseDown===1) {
-                e.target.style.backgroundColor='white';
-              //
-            }
+      
+      
+       
+
     })
  }
 
@@ -44,5 +86,43 @@ for (let i = 0; i < boxs.length; i++) {
  function clearScreen() {
     displyInput1="";
     displyInput2="";
-    disply2.placeholder=displyInput1;
+    displyInput3="";
+    disply2.placeholder=0;
+  }
+
+
+  function addChar(x){
+    displyInput1+=x;
+  }
+
+  function output(){
+     
+    console.log(displyInput1)
+    console.log(displyInput3)
+      let num1=Number(displyInput1);
+      let num2=  Number(displyInput3);
+      let op=Number(operation);
+      clearScreen();
+
+    console.log(displyInput2)
+      switch (operation) {
+        case "+":
+            disply2.placeholder=num1+num2;
+            break;
+        case "-":
+            disply2.placeholder=num1-num2;
+            break;
+        case "/":
+            disply2.placeholder=num1/num2;
+            break;
+        case "x":
+            disply2.placeholder=num1*num2;
+            break;
+          default: disply2.placeholder="error";
+              break;
+      }
+
+      
+      opIndex=0;
+
   }
